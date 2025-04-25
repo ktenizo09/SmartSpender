22d96fd63e3eed76e30bbf4c60240a885d0601c7
ktenizo09 <ktenizo09@gmail.com>
2025-04-25 23:45:35 +0100

    Navigation bar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import ListGroup from "./components/ListGroup"
+import NavBar from "./components/NavBar"
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
-  return <div><ListGroup/></div>
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/budget-calculator" />
+          <Route path="/job-portal" />
+          <Route path="/forum" />
+          <Route path="/about" />
+          <Route path="/contact" />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
 export default App
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBillTrendUp, faCircleUser, faHome, faCalculator, faPhone, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function NavBar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <>
+            <nav className="navigation-bar">
+                <Link to="/" className="navbar-logo">
+                    SmartSpender <FontAwesomeIcon icon={faMoneyBillTrendUp} />
+                </Link>
+
+                {/* <div className="menu-icon" onClick={handleClick}>
+                        <i>{click ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}</i>
+                    </div> */}
+                <ul className='list'>
+                    <li>
+                        <Link to="/account" className="nav-links" >
+                            <FontAwesomeIcon icon={faCircleUser} /> Sign in</Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="nav-links" >
+                            <FontAwesomeIcon icon={faHome} /> Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/budget-calculator" className="nav-links" >
+                            <FontAwesomeIcon icon={faCalculator} /> Budget Calculator</Link>
+                    </li>
+                    <li>
+                        <Link to="/job-portal" className="nav-links">
+                            <FontAwesomeIcon icon={faLinkedin} /> Job Portal</Link>
+                    </li>
+                    <li>
+                        <Link to="/forum" className="nav-links">
+                            <FontAwesomeIcon icon={faCalculator} /> Forum</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="nav-links" >
+                            <FontAwesomeIcon icon={faCircleInfo} /> About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className="nav-links" >
+                            <FontAwesomeIcon icon={faPhone} /> Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
